52c5e6e212937c43bed09ddddccfaddecbb0fdf7
Jiwon <raipen@knu.ac.kr>
2021-07-19 01:16:15 +0900

    clean

--- test.py
+++ test.py
@@ -1,21 +1,13 @@
 from tkinter import *
 
 root = Tk()
-<<<<<<< HEAD
-
-ob_fun ={
-    "a" : c
-    "b" : "k"
-}
 
 def click():
-    print(ob_fun["a"])
+    print("asdf")
 
 btn = Button(root, text="클릭",command=click)
 
 btn.pack()
 
 
-=======
->>>>>>> 5c21d70571273967f2aa52cf537c2bfe4988962e
 root.mainloop()

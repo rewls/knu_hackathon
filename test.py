14f3b9a372f5fb3d496239877b5e6d6d9c04c3cb
Jiwon <raipen@knu.ac.kr>
2021-07-19 02:01:12 +0900

    check requests

--- test.py
+++ test.py
@@ -1,9 +1,12 @@
 from tkinter import *
-
+import requests
+import sys
+print(sys.executable)
 root = Tk()
 
 def click():
-    print("asdf")
+    r = requests.get("http://google.com")
+    print(r.text)
 
 btn = Button(root, text="클릭",command=click)
 

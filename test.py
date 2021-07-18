ed6436e9d8ec799f2ebb2ae4c2be11cb522e937a
Jiwon <raipen@knu.ac.kr>
2021-07-19 01:12:46 +0900

    test ver2 update

--- test.py
+++ test.py
@@ -1,2 +1,18 @@
-print("test")
-print("asdf")
+from tkinter import *
+
+root = Tk()
+
+ob_fun ={
+    "a" : c
+    "b" : "k"
+}
+
+def click():
+    print(ob_fun["a"])
+
+btn = Button(root, text="클릭",command=click)
+
+btn.pack()
+
+
+root.mainloop()

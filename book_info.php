050330ca36b2bd8b72dd0b8fa5172b67c2a2a74d
MOJAN3543 <sjs732@naver.com>
2021-07-20 01:30:45 +0900

    update info

--- book_info.php
+++ book_info.php
@@ -14,13 +14,28 @@
     });
   </script>
   <style>
-    .my-box { border:1px solid; padding: 10px; }
+    @import url('https://fonts.googleapis.com/css2?family=Nanum+Gothic:wght@400;700;800&display=swap');
+    .info-box { border:1px solid silver; padding: 20px; overflow:hidden;}
+    .book-img { border:1px solid; width:80px; height:110.19px; float: left; margin: 5px;, position:static}
+    .book-title { vertical-align: top; text-align: center; padding: 5px; font-family:Nanum Gothic; font-size: 170%; font-weight: bolder; }
+    .book-author { vertical-align: top; text-align: center; padding: 5px; font-family:Nanum Gothic; font-size: 80%; font-weight: 100; }
+    .book-status { vertical-align: top; text-align: center; font-family:Nanum Gothic; font-size: 110%;}
+    .book-detail { vertical-align: top; text-align: center; padding: 5px; font-family:Nanum Gothic; font-size: 120%; color: blue;}
+    .check-icon { padding:10px; position: absolute; top: 0px; right:0px}
   </style>
 </head>
 
   <body>
-    <div class="my-box">
-      Tesst
+    <div class="info-box">
+      <img class="book-img" src="example.jpg" alt="버번 위스키의 모든 것">
+      <span class="material-icons check-icon">star_border</span>
+      <div>
+        <span class="book-title"> 버번 위스키의 모든 것 </span><br>
+        <span class="book-author"> 조승원 / 파주: 싱긋, 2020 </span><br><br>
+        <span class="material-icons">room</span>
+        <span class="book-status"> 4층 자연과학자료실 / 대출 가능 </span><br>
+        <span class="book-detail"> [ 상세 정보 ] </span>
+      </div>
     </div>
   </body>
 </html>

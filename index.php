a9338c0bdee8f83f887308ef90e009c0627a95a4
Jiwon <raipen@knu.ac.kr>
2021-07-19 20:29:28 +0900

    add icon

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
+  <?php
+    include $_SERVER['DOCUMENT_ROOT'].'/broswertest.php';
+  ?>
   <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta property="og:image" content="http://bolgogi.gabia.io/icon.png">

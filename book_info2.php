5c1fc4e65bdde34ed3d0605ec89e65f9a3b31fc0
Jiwon <raipen@knu.ac.kr>
2021-07-19 20:16:15 +0900

    bookinfo2

---
+++ book_info2.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <meta property="og:image" content="http://bolgogi.gabia.io/icon.png">
+  <meta name="viewport" content="width=device-width">
+  <title>경북대 도서관</title>
+  <link rel="stylesheet" href="/top_bar.css">
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <script src="/jquery-3.2.0.min.js"></script>
+  <script type="text/javascript">
+    $(document).ready(function() {
+
+    });
+  </script>
+  <style>
+
+  </style>
+</head>
+  <body>
+
+
+  </body>
+</html>

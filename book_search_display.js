bfd36d6b682e39d4359c8b2cf0b73f0e003340cd
kinggodyujun <kinggodyujun@knu.ac.kr>
2021-07-21 16:37:53 +0900

    add BookLocation js

---
+++ book_search_display.js
@@ -0,0 +1,176 @@
+var result_html = "";
+var PreviousBook="";
+var BookLocData = "";
+var BookStateData = "";
+
+var SearchBookLoc = function(BookID){
+    //console.log(BookID);
+    $.ajax({
+      url:'/book_location.php',
+      async:false,
+      type:'GET',
+      data:{id:BookID},
+      success:function(data){
+        data = JSON.parse(data);
+        //console.log(data);
+        if (data.isJungDo == false){
+          BookLocData = "상주캠퍼스";
+          BookStateData = "대출가능";
+          //console.log([BookLocData, BookStateData]);
+          //console.log(data.isJungDo);
+        }
+        else{
+          //console.log(data.list[0].location);
+          BookLocData = data.list[0].location;
+          BookStateData = data.list[0].state;
+        }
+      }
+    });
+}
+
+function Search(cnt){
+  var max_int = 20;
+  var ParsedData = "";
+  if($(".search_bar>input").val()==""){
+    alert("검색어를 입력해주세요");
+    return;
+  }
+  if (PreviousBook != $(".search_bar>input").val()){
+    result_html = "";
+  }
+  var type = $(".drop_result").text();
+  var type_arr={전체:"all",제목:"title",저자:"author",출판사:"publisher"};
+  PreviousBook = $(".search_bar>input").val()
+  $.ajax({
+    url:'/book_search.php',
+    type:'POST',
+    data:{type:type_arr[type],
+      name:$(".search_bar>input").val(),
+      max:20,
+      offset:cnt*max_int},
+      success:function(data){
+      ParsedData = JSON.parse(data);
+      var temp_imagechecker = '';
+      for(var i=0;i<ParsedData.list.length;i++){
+
+        SearchBookLoc(ParsedData.list[i].id);
+        var Arry = [BookLocData, BookStateData];
+
+        temp_imagechecker = ParsedData.list[i].imgUrl ? ParsedData.list[i].imgUrl : "img/NoUrl.jpg"
+        result_html = result_html + '<div class="info-box">'+
+      '<img class="book-img" src="'+ temp_imagechecker +'" alt="'+ParsedData.list[i].title+'">'
+        +'<span class="material-icons check-icon">star_border</span>'
+        +'<div>'
+          +'<span class="book-title"> '+ParsedData.list[i].title+' </span>'
+          +'<span class="book-author"> '+ParsedData.list[i].author+' / '+ParsedData.list[i].publication+' </span>'
+          +'<span class="material-icons">room</span>'
+          +'<span class="book-status"> '+Arry[0]+' / '+Arry[1]+' </span>'
+          +'<span class="book-detail"> [ 상세 정보 ] </span>'
+        +'</div>'
+      +'</div>';
+      
+      }
+      if (ParsedData.list.length < max_int){
+        $("#contents").html(result_html);
+      }
+      else{
+        $("#contents").html(result_html+'<p style="margin-top: 20px;"><div id="more" style="text-align:center;"><strong onclick="Search('+cnt+1+')" style="cursor:pointer">더보기</strong></div></p>');
+      }
+      $(".book-title").click(function(){
+        $(this).parent('div').toggleClass("full");
+      });
+    }
+  })
+}
+/*
+function FirstSearch(){
+    if($(".search_bar>input").val()==""){
+      alert("검색어를 입력해주세요");
+      return;
+    }
+    if (firstBook != $(".search_bar>input").val()){
+      result_html = "";
+    }
+    var type = $(".drop_result").text();
+    var type_arr={전체:"all",제목:"title",저자:"author",출판사:"publisher"};
+    firstBook = $(".search_bar>input").val()
+    $.ajax({
+      url:'/book_search.php',
+      type:'POST',
+      data:{type:type_arr[type],
+        name:$(".search_bar>input").val(),
+        max:20,
+        offset:0},
+        success:function(data){
+          SearchData = JSON.parse(data);
+          var temp_imagechecker = '';
+          for(var i=0;i<SearchData.list.length;i++){
+            BookLocData = SearchBookLoc(SearchData.list[i].id)
+            //console.log(BookLocData);
+            temp_imagechecker = SearchData.list[i].imgUrl ? SearchData.list[i].imgUrl : "img/NoUrl.jpg"
+            result_html = result_html + '<div class="info-box">'+
+            '<img class="book-img" src="'+ temp_imagechecker +'" alt="'+SearchData.list[i].title+'">'
+              +'<span class="material-icons check-icon">star_border</span>'
+              +'<div>'
+                +'<span class="book-title"> '+SearchData.list[i].title+' </span>'
+                +'<span class="book-author"> '+SearchData.list[i].author+' / '+SearchData.list[i].publication+' </span>'
+                +'<span class="material-icons">room</span>'
+                +'<span class="book-status"> '+BookLocData.list[0].location+' / '+BookLocData.list[0].state+' </span>'
+                +'<span class="book-detail"> [ 상세 정보 ] </span>'
+              +'</div>'
+            +'</div>';
+          }
+          if (SearchData.list.length < 20){
+            $("#contents").html(result_html);
+          }
+          else{
+            $("#contents").html(result_html+'<p style="margin-top: 20px;"><div id="more" style="text-align:center;"><strong onclick="SearchMore('+i+')" style="cursor:pointer">더보기</strong></div></p>');
+          }
+          $(".book-title").click(function(){
+            $(this).parent('div').toggleClass("full");
+          });
+        }
+    })
+}
+function SearchMore(cnt){
+    if (firstBook != $(".search_bar>input").val()){
+      result_html = "";
+      j = 0
+    }
+    var type = $(".drop_result").text();
+    var type_arr={전체:"all",제목:"title",저자:"author",출판사:"publisher"};
+    firstBook = $(".search_bar>input").val();
+    $.ajax({
+      url:'/book_search.php',
+      type:'POST',
+      data:{type:type_arr[type],
+        name:$(".search_bar>input").val(),
+        max:20,
+        offset:cnt},
+        success:function(data){
+          SearchData = JSON.parse(data);
+          var temp_imagechecker = '';
+          for(var i=0; i<20; i++){
+            BookLocData = SearchBookLoc(SearchData.list[i].id)
+            j += 1
+            temp_imagechecker = SearchData.list[i].imgUrl ? SearchData.list[i].imgUrl : "img/NoUrl.jpg"
+            result_html = result_html + '<div class="info-box">'+
+            '<img class="book-img" src="'+ temp_imagechecker +'" alt="'+SearchData.list[i].title+'">'
+              +'<span class="material-icons check-icon">star_border</span>'
+              +'<div>'
+                +'<span class="book-title"> '+SearchData.list[i].title+' </span>'
+                +'<span class="book-author"> '+SearchData.list[i].author+' / '+SearchData.list[i].publication+' </span>'
+                +'<span class="material-icons">room</span>'
+                +'<span class="book-status"> '+BookLocData.list.location+' / '+BookLocData.list.state+' </span>'
+                +'<span class="book-detail"> [ 상세 정보 ] </span>'
+              +'</div>'
+            +'</div>';
+          }
+          $("#contents").html(result_html+'<p style="margin-top: 20px;"><div id="more" style="text-align:center;"><strong onclick="SearchMore('+j+')" style="cursor:pointer">더보기</strong></div></p>');
+          $(".book-title").click(function(){
+            $(this).parent('div').toggleClass("full");
+          });
+        }
+    });
+}
+*/

b5afb793c0942e351314eee4ed41c63cc2b1973b
MOJAN3543 <sjs732@naver.com>
2021-07-21 13:40:28 +0900

    Minimize algorithm

--- js/book_search_display.js
+++ js/book_search_display.js
@@ -17,6 +17,53 @@ function SearchBookLoc(BookID){
       }
     });
 }
+function Search(cnt){
+  var result_html = "";
+  var ParsedData = "";
+  var PreviousBook="";
+  if($(".search_bar>input").val()==""){
+    alert("검색어를 입력해주세요");
+    return;
+  }
+  if (PreviousBook != $(".search_bar>input").val()){
+    result_html = "";
+  }
+  var type = $(".drop_result").text();
+  var type_arr={전체:"all",제목:"title",저자:"author",출판사:"publisher"};
+  PreviousBook = $(".search_bar>input").val()
+  $.ajax({
+    url:'/book_search.php',
+    type:'POST',
+    data:{type:type_arr[type],
+      name:$(".search_bar>input").val(),
+      max:20,
+      offset:cnt*max},
+      success:function(data){
+      ParsedData = JSON.parse(data);
+      var temp_imagechecker = '';
+      for(var i=0;i<ParsedData.list.length;i++){
+        temp_imagechecker = ParsedData.list[i].imgUrl ? ParsedData.list[i].imgUrl : "img/NoUrl.jpg"
+        result_html = result_html + '<div class="info-box">'+
+      '<img class="book-img" src="'+ temp_imagechecker +'" alt="'+ParsedData.list[i].title+'">'
+        +'<span class="material-icons check-icon">star_border</span>'
+        +'<div>'
+          +'<span class="book-title"> '+ParsedData.list[i].title+' </span>'
+          +'<span class="book-author"> '+ParsedData.list[i].author+' / '+ParsedData.list[i].publication+' </span>'
+          +'<span class="material-icons">room</span>'
+          +'<span class="book-status"> 4층 자연과학자료실 / 대출 가능 </span>'
+          +'<span class="book-detail"> [ 상세 정보 ] </span>'
+        +'</div>'
+      +'</div>';
+      }
+      if (ParsedData.list.length < max){
+        $("#contents").html(result_html);
+      }
+      else{
+        $("#contents").html(result_html+'<p style="margin-top: 20px;"><div id="more" style="text-align:center;"><strong onclick="Search('+cnt+1+')" style="cursor:pointer">더보기</strong></div></p>');
+      }
+    }
+  })
+}
 
 function FirstSearch(){
     if($(".search_bar>input").val()==""){
